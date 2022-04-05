5dcf322bcf6568c34daa1cb7488ba6ca2cf60c4f
gulphater <dotanoobbich@mail.ru>
2022-04-05 19:20:45 -0400

    initial commit

---
+++ WebScraper/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO; 
+
+namespace WebScraper
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // create a List of string type to store the returned data from scraper object
+            List<string> fetched_data = new List<string>();
+
+
+            // this is the object from the scraper class we have created
+            ScrapingLogic scraper = new ScrapingLogic();
+
+            // a variable to store the url of a web page to be scraped
+            string url = "https://www.usf.edu/engineering/"; 
+
+            // use the method extractNodes() to return the list of nodes' innerHTML
+            fetched_data = scraper.extractNodes(url);
+
+            // Print the data in the console
+            Console.WriteLine(fetched_data);
+
+            // Use your path from root to create a file
+            File.WriteAllLines("/home/shoh/kayf/WebScraper/RESULTS_2.txt", fetched_data); 
+        }
+    }
+}
---
+++ WebScraper/ScrapingLogic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using HtmlAgilityPack;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace WebScraper
+{
+    public class ScrapingLogic
+    {
+        public List<string> extractNodes(string url) {
+            // List will contain the nodes
+            List<string> extracted_nodes = new List<string>();
+
+            // an object DOM to crawl and extract data from web pages
+            HtmlWeb web = new HtmlWeb();
+
+            // the DOM loadds a web pages from a url and stores it in variable
+            var HTML_doc = web.Load(url);
+
+            // specify what nodes from a web page we want to fetch data from
+            var nodes = HTML_doc.DocumentNode.SelectNodes("/html/body");
+  
+
+            // Convert each element in nodes into string
+            foreach (var node in nodes)
+            {
+                extracted_nodes.Add(node.InnerHtml);
+            }
+
+            return extracted_nodes;
+        } 
+    }
+}
